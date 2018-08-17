c527fa0cc7e21eb14ec7213bcd073812fe7f9515
Eyal Avidan <avidan.eyal@gmail.com>
2018-08-17 14:42:08 +0300

    .

--- umt_model.py
+++ umt_model.py
@@ -55,8 +55,7 @@ class UmtModel(nn.Module):
 
         # Run through encoder
         enc = self.encoder.forward(input)
-        latent = F.avg_pool1d(enc.unsqueeze(1).type(
-            self.dtype), kernel_size=POOL_KERNEL)
+        latent = F.avg_pool1d(enc, kernel_size=POOL_KERNEL)
 
         return latent
 
