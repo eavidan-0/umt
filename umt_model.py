7c6a10b121f7dc91a744772206d5dede2d82ded5
Eyal Avidan <avidan.eyal@gmail.com>
2018-08-21 23:09:32 +0300

    .

--- umt_model.py
+++ umt_model.py
@@ -66,12 +66,10 @@ class UmtModel(nn.Module):
         decoder_input = upsampled_latent
 
         # Run through domain decoder
-        decoder_input = mu_law_encode(decoder_input, self.classes)
         out = self.decoders[domain_index].forward(upsampled_latent)
 
         # TODO: ahem? mu? sampling rate? what what?
         out = F.interpolate(out, size=SR, mode='nearest')
-        out = mu_law_encode(out, self.classes)
         out = mu_law_decode(out, self.classes)
 
         return out
