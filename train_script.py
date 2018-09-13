b47e09af2e6363d0f88938052673569ab4b61236
Eyal Avidan <avidan.eyal@gmail.com>
2018-09-13 15:13:59 +0300

    .

--- train_script.py
+++ train_script.py
@@ -48,7 +48,7 @@ for domain_index in range(len(DOMAINS)):
 trainer = UmtTrainer(model=model,
                      datasets=datasets,
                      snapshot_path='snapshots',
-                     snapshot_name='umt_en_nsynth',
+                     snapshot_name='umt',
                      dtype=dtype,
                      ltype=ltype)
 
